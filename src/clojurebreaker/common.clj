38e3afd9f131df20a5f5b43f9fb4b72ad84b1809
Bjorn A. Taranger <bjorn.taranger@gmail.com>
2013-04-30 22:29:41 +0200

    Added the defpartial macro and wired in the input form

--- src/clojurebreaker/common.clj
+++ src/clojurebreaker/common.clj
@@ -1,5 +1,6 @@
 (ns clojurebreaker.common
-  (:use [hiccup.def :only [defhtml]]
+  (:use [hiccup.core :only [html]]
+        [hiccup.def :only [defhtml]]
         [hiccup.page :only [include-css]]))
 
 (defhtml layout [& body]
@@ -7,3 +8,12 @@
    [:title "Welcome to ClojureBreaker!"]
    (include-css "/css/screen.css")]
   (into [:body] body))
+
+
+(defmacro defpartial
+  "Create a function that returns html using hiccup. The function is
+   callable with the given name."
+  [fname params & body]
+  `(defn ~fname ~params
+     (html
+      ~@body)))
--- src/clojurebreaker/handler.clj
+++ src/clojurebreaker/handler.clj
@@ -1,21 +1,31 @@
 (ns clojurebreaker.handler
   (:use [compojure.core]
         [ring.middleware.file :only [wrap-file]]
-;        [ring.middleware.session]
         [sandbar.stateful-session]
-        [clojurebreaker.models.game])
+        [hiccup.form])
   (:require [compojure.route :as route]
+            [clojurebreaker.models.game :as game]
             [clojurebreaker.common :as common]
             [ring.middleware.stacktrace :as trace]))
 
 
+(common/defpartial board []
+  (form-to [:post "/guess"]
+           (text-field "one")
+           (text-field "two")
+           (text-field "three")
+           (text-field "four")
+           (submit-button "Guess")))
+
+
 (defn welcome []
-  (when-not (session-get :game) (session-put! :game (create)))
+  (when-not (session-get :game) (session-put! :game (game/create)))
   (let [counter (+ 1 (session-get :counter 0))]
     (do (session-put! :counter counter)
         (common/layout
          [:p "Stateful clojurebreaker " counter]
-         [:p "Clojurebreaker game: " (session-get :game)]))))
+         [:p "Clojurebreaker game: " (session-get :game)]
+         (board)))))
 
 (defroutes app-routes
   (GET "/" [] (welcome))
